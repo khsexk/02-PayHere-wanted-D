70edb60bc137522051dc653151a3bbad8b5fe73e
khsexk <kohyunsuk98@gmail.com>
2022-07-07 23:34:39 +0900

    Update: app 모듈 import 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import * as ormConfig from '../ormConfig';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot(ormConfig), UsersModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
